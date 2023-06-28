fc224410b84972ce648a1947625f92c314f4a0da
DESKTOP-7RO48O1\kolot <kolotseidenis@gmail.com>
2023-06-28 12:13:15 +0300

    export axios

--- src/hooks/useUploadFile.ts
+++ src/hooks/useUploadFile.ts
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
-
-const API_KEY='kolotseidenis@gmail.com_3ca171f94542e1471c19008d0d6be9e2fdb516915ec421e3e9b92774881efb3fccb3dd2a';
+import { generateLink, uploadFile } from '../pdfAPI';
 
 function useUploadFile()
 {
@@ -17,19 +15,8 @@ function useUploadFile()
     useEffect(()=>{
         if(!loadedFile) return;
 
-        const headers = {
-            'x-api-key': API_KEY,
-        };
-
-        const params = {
-            'name': 'file.pdf',
-        };
-
         try{
-            axios.get('https://api.pdf.co/v1/file/upload/get-presigned-url', {
-                headers,
-                params,
-            }).then((res: any)=>{
+            generateLink().then((res: any)=>{
                 setPresignedUrl(res.data.presignedUrl);
                 setUrl(res.data.url);
                 console.log(res.data.presignedUrl)
@@ -43,22 +30,10 @@ function useUploadFile()
 
      useEffect(()=>{
         if(presignedUrl.length === 0) return;
-
-        const headers = {
-            'x-api-key': API_KEY,
-            'Content-Type': 'application/octet-stream',
-        };
+        if(!loadedFile) return;
 
         try{
-        /*await axios.put(presignedUrl, loadedFile, {
-            headers: headers
-        });*/
-            axios({
-                method: 'PUT',
-                url: presignedUrl,
-                data: loadedFile,
-                headers: headers
-            }).then(()=>{
+            uploadFile(presignedUrl, loadedFile).then(()=>{
                 setUploading(false);
             });
         } catch(error: any) {
---
+++ src/pdfAPI.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const API_KEY='kolotseidenis@gmail.com_3ca171f94542e1471c19008d0d6be9e2fdb516915ec421e3e9b92774881efb3fccb3dd2a';
+
+interface Links
+{
+    "presignedUrl": string;
+    "url": string;
+    "error": boolean;
+    "status": number;
+    "name": string;
+    "remainingCredits": number;
+}
+
+export async function generateLink(){
+    const headers = {
+        'x-api-key': API_KEY,
+    };
+
+    const params = {
+        'name': 'my_pdf.pdf',
+    };
+
+    const res = axios.get<Links>('https://api.pdf.co/v1/file/upload/get-presigned-url', {
+        headers,
+        params,
+    })
+
+    return res;
+} 
+
+export async function uploadFile(presignedUrl: string, file: Uint8Array){
+    const headers = {
+        'x-api-key': API_KEY,
+        'Content-Type': 'application/octet-stream',
+    };
+
+    /*await axios.put(presignedUrl, loadedFile, {
+        headers: headers
+    });*/
+    const res = axios({
+        method: 'PUT',
+        url: presignedUrl,
+        data: file,
+        headers: headers
+    })
+
+    return res;
+}
